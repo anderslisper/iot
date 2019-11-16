c8c4c6ecd2fdb8a8b65d44577ea05b9630b8eb87
Anders Lisper <45653440+anderslisper@users.noreply.github.com>
2019-11-16 14:59:35 +0100

    Get IP

--- IotDevice.py
+++ IotDevice.py
@@ -167,11 +167,18 @@ class IotDevice:
             pass
 
         return False
-        
+
     def getIpAddress(self):
-        hostname = socket.gethostname()    
-        IPAddr = socket.gethostbyname(hostname)    
-        return IPAddr
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        try:
+            # doesn't even have to be reachable
+            s.connect(('10.255.255.255', 1))
+            IP = s.getsockname()[0]
+        except:
+            IP = '127.0.0.1'
+        finally:
+            s.close()
+        return IP
 
     # Main loop
     def main_loop(self):
